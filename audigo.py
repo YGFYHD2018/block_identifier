cc285df75ff7710723a445ca024fd98ac5648648
code560 <smartphone.560@gmail.com>
2018-11-08 09:41:09 +0900

    mod

--- audigo.py
+++ audigo.py
@@ -7,5 +7,9 @@ import json
 def play(src):
     url = 'http://audigo.local/audio/v1/play/block'
     d = json.dumps({"src": src})
-    requests.post(url, data=d, timeout=0.1)
+    try:
+        requests.post(url, data=d, timeout=0.1)
+    except:
+        import traceback
+        traceback.print_exc()
 
